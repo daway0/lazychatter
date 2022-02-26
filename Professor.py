d3f108f936caa79d4c8c25a5d8f64a3c99c699a5
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-02-26 14:26:07 +0330

    Initial

---
+++ Professor.py
@@ -0,0 +1,16 @@
+
+from Student import Student
+
+
+class Professor:
+    def __init__(self) -> None:
+        self.__students = {}
+
+    def add_student(self, student: Student) -> None:
+        self.__students[student.name()] = student
+
+    def get_student(self, student_name) -> Student:
+        return self.__students[student_name]
+
+    def student_name_list(self):
+        return self.__students.keys()
