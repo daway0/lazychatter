0c2fc3918001cf0c71748e7188dff47bd86aa008
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-02-28 15:56:40 +0330

    feat(Export.py): pie plot format added. start using matplotlib

--- Export.py
+++ Export.py
@@ -17,6 +17,30 @@ class DataExport:
         pass
 
     @staticmethod
+    def pieplot(professor: Professor):
+        import matplotlib.pyplot as plt
+        import matplotlib
+        from bidi.algorithm import get_display
+        import arabic_reshaper
+
+        name_list = professor.student_name_list()
+        reshaped_names_list = []
+        for name in name_list:
+            reshaped_text = arabic_reshaper.reshape(name)
+            artext = get_display(reshaped_text)
+            reshaped_names_list.append(artext)
+
+        number_chats_list = []
+        for name in professor.student_name_list():
+            number_student_chats = professor.get_student(
+                name).number_all_chats()
+            number_chats_list.append(number_student_chats)
+
+        plt.pie(number_chats_list, labels=reshaped_names_list,
+                radius=1.4, autopct='%1.1f%%', pctdistance=0.8)
+        plt.show()
+
+    @staticmethod
     def overall_result(directory, files, professor: Professor) -> None:
         workbook = xlsxwriter.Workbook(f'./{directory}/overall_result.xlsx')
         worksheet = workbook.add_worksheet()
