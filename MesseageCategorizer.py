0486d41cec11ffc3054851acfbcf18facac72694
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-03-01 17:31:54 +0330

    style: adjust whitespaces

--- MesseageCategorizer.py
+++ MesseageCategorizer.py
@@ -13,11 +13,11 @@ class ChatMesseageCategorizer:
     def categorize(self) -> None:
         for line in self.__chat.chat_lines:
             student_name = ChatDataExtractor.message_author(line)
-            
+
             if not student_name in self.__chat_professor.student_name_list():
                 student = Student(student_name)
                 self.__chat_professor.add_student(student)
-            
+
             text = ChatDataExtractor.text_with_time(line)
             self.__chat_professor.get_student(student_name).add_message(
                 text, self.__chat.class_date())
