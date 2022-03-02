3b144fe8a7e4e3ade0070d04a13c7589a09e3d58
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-03-02 18:50:45 +0330

    feat(DirectoryManager.py): now we can remove directories of last execution

--- DirectoryManager.py
+++ DirectoryManager.py
@@ -1,19 +1,24 @@
 import os
+import shutil
 
 
 class DirectoryManager:
 
     @staticmethod
+    def __remove_directory(directory: str):
+        try:
+            shutil.rmtree(directory)
+        except:
+            pass
+
+    @staticmethod
     def make_directories(directories_name_list: list) -> None:
-        os.system('cls')
+        
         for name in directories_name_list:
-            try:
-                os.mkdir(f'./{name}')
+            DirectoryManager.__remove_directory(name)
+            os.mkdir(f'./{name}')
 
-            except:
-                pass
-
-        os.system('cls')
+        
 
     @staticmethod
     def import_chat_files() -> list:
