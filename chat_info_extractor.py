f86449549e86f90f5c6810d81f5a02091e912eb4
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-03-09 03:01:12 +0330

    feat: extracting start and end time of chat

--- chat_info_extractor.py
+++ chat_info_extractor.py
@@ -1,4 +1,5 @@
 from chat import Chat
+from message import Message
 
 
 class ChatInfoExtractor:
@@ -16,3 +17,12 @@ class ChatInfoExtractor:
         cursor_start_location = file_name.index('-')+1
         curosr_stop_location = file_name.index('[')
         return file_name[cursor_start_location:curosr_stop_location]
+
+    @staticmethod
+    def start_end(chat: Chat) -> tuple:
+        with open(chat.file_name, 'r', encoding='utf-8') as f:
+            first_msg = Message(f.readline())
+            for last_line in f:
+                pass
+            last_msg = Message(last_line)
+            return (first_msg.time(), last_msg.time())
