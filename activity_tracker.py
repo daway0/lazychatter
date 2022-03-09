368a0306888cc3be2b3468fd5c36778faef542d7
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-03-09 18:45:33 +0330

    ActivityTracker class added

---
+++ activity_tracker.py
@@ -0,0 +1,19 @@
+from professor import Professor
+
+
+class ActivityTracker():
+    @staticmethod
+    def all_student_activity(professor: Professor, timeline: list[str]) -> dict:
+        timeline_msg_dict = {}
+        # {'16:20': 5,
+        #  '16:21': 1,
+        #  '16:30': 0, ...}
+
+        for time in timeline:
+            number_msgs = 0
+            for std_name in professor.student_name_list():
+                student = professor.get_student(std_name)
+                number_msgs += student.msgs_in_time(time)
+            timeline_msg_dict[time] = number_msgs
+
+        return timeline_msg_dict
