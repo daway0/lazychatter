d6ce23b42cb2cbee7bb5f5f30b468d84b3418968
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-03-13 12:26:17 +0330

    chore(activity_tracker): track msgs in timeline in spicific class date (instead of all classes)

--- activity_tracker.py
+++ activity_tracker.py
@@ -3,7 +3,7 @@ from professor import Professor
 
 class ActivityTracker():
     @staticmethod
-    def all_students_activity(professor: Professor, timeline: list[str]) -> dict:
+    def all_students_activity(professor: Professor, timeline: list[str], class_date) -> dict:
         timeline_msg_dict = {}
         # {'16:20': 5,
         #  '16:21': 1,
@@ -13,7 +13,7 @@ class ActivityTracker():
             number_msgs = 0
             for std_name in professor.student_name_list():
                 student = professor.get_student(std_name)
-                number_msgs += student.msgs_in_time(time)
+                number_msgs += student.msgs_in_time(time, class_date)
             timeline_msg_dict[time] = number_msgs
 
         return timeline_msg_dict
