898d9e824f057bff217bd2298d1326c9361ba952
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-03-14 23:35:04 +0330

    bigfeat(activity_drawer.py): adding StudentActivityDrawer - activity behaviour

--- activity_drawer.py
+++ activity_drawer.py
@@ -1,5 +1,9 @@
+from textwrap import indent
 from PIL import ImageDraw, Image
 import math
+from professor import Professor
+from student import Student
+from timeline import Timeline
 
 
 class Board:
@@ -86,3 +90,65 @@ class ClassActivityDrawer:
                 max = self.__data[date][time]
 
         self.__opacity_ratio = math.ceil(255/max)
+
+
+class StudentActivityDrawer:
+    def __init__(self, professor: Professor, timeline: list) -> None:
+        self.__professor = professor
+        self.__timeline = timeline
+
+    def draw(self, detail: bool = False):
+        indent = 50
+        line_number_x = int(indent/2)
+        width = 1920
+        width_indent = width+indent*2
+        length = width / len(self.__timeline)
+        height = int(length * (len(self.__professor.student_name_list())+1))
+        board = Board(width_indent, height, color_fill='black').build()
+        db = ImageDraw.Draw(board)
+
+        y = 0
+        line_number = 0
+        for (_, student), msgs in self.__professor.sorted_name_list()[::-1]:
+
+            line_number += 1
+            x = 0
+            db.text((x, y), str(msgs))
+            opacity_ratio = self.__cal_opacity(student)
+            x = indent
+            for time in self.__timeline:
+                std_msgs = student.msgs_in_time(time)
+                if not std_msgs == 0:
+
+                    opacity = std_msgs * opacity_ratio
+                    sq = Square(length, (0, opacity, 0))
+                    sq.draw(x, y, db)
+                    if detail:
+                        db.text(
+                            (x, y), f'{std_msgs}', fill='white')
+                x += length
+            db.text((line_number_x, y), f'>{line_number}', fill='yellow')
+            db.text((width+indent, y), f'{line_number}<', fill='yellow')
+            db.text((int((width_indent)/2), y),
+                    f'<{line_number}>', fill=(100, 100, 100))
+            db.rectangle((indent, y, width, y), fill=(40, 40, 40))
+            y += length
+        x = indent
+        db.rectangle((indent, y, width+indent, y), fill='white')
+        # wihte line
+        y += int(length/3)
+        for time in self.__timeline:
+            if self.__timeline.index(time) % 10 == 0:
+                db.text((x, y), f"{time}'")
+            if self.__timeline.index(time) == len(self.__timeline)-1:
+                db.text((x-length, y), f"{time[3:]}'")
+            x += length
+        if detail:
+            board.save('students-activity-detail.png')
+        else:
+            board.save('students-activity.png')
+        board.show()
+
+    def __cal_opacity(self, student: Student):
+        max = student.max_msgs_in_min()
+        return math.ceil(150/max)
