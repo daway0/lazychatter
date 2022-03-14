e50ec3d1bd5656436667dacda7e108f0992003f6
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2022-03-14 23:31:47 +0330

    feat(professor.py): adding sorted_name_list() for student names

--- professor.py
+++ professor.py
@@ -1,4 +1,6 @@
 from student import Student
+from bidi.algorithm import get_display
+import arabic_reshaper
 
 
 class Professor:
@@ -13,3 +15,35 @@ class Professor:
 
     def student_name_list(self):
         return self.__students.keys()
+
+    def sorted_name_list(self):
+        name_list = self.student_name_list()
+        reshaped_names_list = []
+
+        for name in name_list:
+            reshaped_text = arabic_reshaper.reshape(name)
+            artext = get_display(reshaped_text)
+            reshaped_names_list.append(
+                ((f'{artext} [{self.get_student(name).number_all_chats()} in {self.get_student(name).number_all_classes()}]'), self.get_student(name)))
+
+        number_chats_list = []
+        all_chats = 0
+        for name in self.student_name_list():
+            number_student_chats = self.get_student(
+                name).number_all_chats()
+            all_chats += number_student_chats
+            number_chats_list.append(number_student_chats)
+
+        # sorting data by number of chat messages
+        #
+        desorted_list_tuple = []
+        for i in range(len(number_chats_list)):
+            desorted_list_tuple.append(
+                (reshaped_names_list[i], number_chats_list[i]))
+        sorted_list_tuple = self.__sort_tuple(desorted_list_tuple)
+
+        return sorted_list_tuple
+
+    def __sort_tuple(self, tup):
+        tup.sort(key=lambda x: x[1])
+        return tup
